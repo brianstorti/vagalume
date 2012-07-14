a6132b9a83cd76cbf28cb26a7d87be44299aa99c
Brian Thomas Storti <btstorti@gmail.com>
2012-07-14 17:49:56 -0300

    bug fix: handle cases then there is no translation available

--- lib/vagalume/search_result.rb
+++ lib/vagalume/search_result.rb
@@ -12,7 +12,7 @@ module Vagalume
       return result if result.status == Vagalume::Status::NOT_FOUND || result.status == Vagalume::Status::SONG_NOT_FOUND
       song = result_json["mus"].first
       artist = result_json["art"]
-      translations = song["translate"]
+      translations = song["translate"] || []
       result.song = Vagalume::Song.fetch(song)
       result.artist = Vagalume::Artist.fetch(artist)
 
--- spec/lyric_formatter_spec.rb
+++ spec/lyric_formatter_spec.rb
@@ -3,23 +3,36 @@
 require "spec_helper"
 
 describe Vagalume::LyricFormatter do
-  before(:each) do
-    VCR.use_cassette('vagalume') do
-      @search = Vagalume.find("Metallica", "The Unforgiven")
+  context "there is a translation available" do
+    before(:each) do
+      VCR.use_cassette('vagalume') do
+        @search = Vagalume.find("Metallica", "The Unforgiven")
+      end
+    end
+
+    it "should format song without translation option" do
+      options = {:translation => false}
+      formatted_lyric = Vagalume::LyricFormatter.format(@search, options)
+      lyric = File.read 'spec/assets/lyric_formatter/unforgiven_lyric.txt'
+      formatted_lyric.should == lyric
+    end
+
+    it "should format song with translation" do
+      options = {:translation => true}
+      formatted_lyric = Vagalume::LyricFormatter.format(@search, options)
+      lyric = File.read 'spec/assets/lyric_formatter/unforgiven_lyric_with_translation.txt'
+      formatted_lyric.should == lyric
     end
   end
 
-  it "should format song without translation" do
-    options = {:translation => false}
-    formatted_lyric = Vagalume::LyricFormatter.format(@search, options)
-    lyric = File.read 'spec/assets/lyric_formatter/unforgiven_lyric.txt'
-    formatted_lyric.should == lyric
-  end
-
-  it "should format song with translation" do
-    options = {:translation => true}
-    formatted_lyric = Vagalume::LyricFormatter.format(@search, options)
-    lyric = File.read 'spec/assets/lyric_formatter/unforgiven_lyric_with_translation.txt'
-    formatted_lyric.should == lyric
+  context "there is no translation available" do
+    it "should inform user there is no translation available" do
+      VCR.use_cassette('vagalume_with_no_translation_available') do
+        @search = Vagalume.find("Bruce Springsteen", "Ain't good enough for You")
+        options = {:translation => true}
+        output = Vagalume::LyricFormatter.format(@search, options)
+        output.should == "No translation found"
+      end
+    end
   end
 end
