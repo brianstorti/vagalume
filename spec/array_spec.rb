8678cf620af0e15602d2bb86e71f1e30054a7964
Brian Thomas Storti <btstorti@gmail.com>
2014-04-08 17:36:19 -0300

    change deprecated stub!

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -5,10 +5,10 @@ require "spec_helper"
 describe Array do
   it "return song with selected language" do
     right_song = double "Song"
-    right_song.stub!(:language).and_return(Vagalume::Language::PORTUGUESE)
+    right_song.stub(:language).and_return(Vagalume::Language::PORTUGUESE)
 
     wrong_song = double "Song"
-    wrong_song.stub!(:language).and_return(Vagalume::Language::ENGLISH)
+    wrong_song.stub(:language).and_return(Vagalume::Language::ENGLISH)
 
     songs = [right_song, wrong_song]
     songs.with_language(Vagalume::Language::PORTUGUESE).should == right_song
