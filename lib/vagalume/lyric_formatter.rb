2ede1f08a504d61c48f9e8878187b0a05b8dedcb
Brian Thomas Storti <btstorti@gmail.com>
2015-02-28 20:13:59 -0300

    keep consitency with module/class approach

--- lib/vagalume/lyric_formatter.rb
+++ lib/vagalume/lyric_formatter.rb
@@ -1,38 +1,40 @@
-class Vagalume::LyricFormatter
-  def format(search, options)
-    return "No lyric found" if search.not_found?
+module Vagalume
+  class LyricFormatter
+    def format(search, options)
+      return "No lyric found" if search.not_found?
 
-    song = search.song
-    translated_song = search.translations.with_language(Vagalume::Language::PORTUGUESE)
-    output = "\n\n"
+      song = search.song
+      translated_song = search.translations.with_language(Vagalume::Language::PORTUGUESE)
+      output = "\n\n"
 
-    return "\n\n#{song.name}\n\n#{song.lyric}" unless options[:translation]
-    return "No translation found" if translated_song.nil?
+      return "\n\n#{song.name}\n\n#{song.lyric}" unless options[:translation]
+      return "No translation found" if translated_song.nil?
 
-    bigger_line = bigger_line(song.lyric)
-    lyric_array = [song.name, ""]
-    translation_array = [translated_song.name, ""]
+      bigger_line = bigger_line(song.lyric)
+      lyric_array = [song.name, ""]
+      translation_array = [translated_song.name, ""]
 
-    lyric_array += song.lyric.split("\n")
-    translation_array += translated_song.lyric.split("\n")
+      lyric_array += song.lyric.split("\n")
+      translation_array += translated_song.lyric.split("\n")
 
-    lyric_array.each_with_index do |lyric_line, index|
-      output += lyric_line + separator(bigger_line, lyric_line) + translation_array[index].to_s + "\n"
+      lyric_array.each_with_index do |lyric_line, index|
+        output += lyric_line + separator(bigger_line, lyric_line) + translation_array[index].to_s + "\n"
+      end
+      output
     end
-    output
-  end
 
-  private
+    private
 
-  def separator(bigger_line, lyric_line)
-    " " * (bigger_line - lyric_line.size) + "     |     "
-  end
-
-  def bigger_line(lyric)
-    bigger = 0
-    lyric.each_line do |line|
-      bigger = line.size if line.size > bigger
+    def separator(bigger_line, lyric_line)
+      " " * (bigger_line - lyric_line.size) + "     |     "
     end
-    bigger
+
+    def bigger_line(lyric)
+      bigger = 0
+      lyric.each_line do |line|
+        bigger = line.size if line.size > bigger
+      end
+      bigger
+    end
   end
 end
