b293e11fb1b4f5b7014699e0954786f23c3d6e57
Brian Thomas Storti <btstorti@gmail.com>
2015-03-01 14:39:32 -0300

    better names

--- lib/vagalume/lyric_formatter.rb
+++ lib/vagalume/lyric_formatter.rb
@@ -4,9 +4,9 @@ module Vagalume
       return "No lyric found" if search.not_found?
 
       original_song = search.song
-      translated_song = search.translations.with_language(Vagalume::Language::PORTUGUESE)
 
       if options[:translation]
+        translated_song = search.translations.with_language(Vagalume::Language::PORTUGUESE)
         return "No translation found" if translated_song.nil?
         return formatted_song_with_transaction(original_song, translated_song)
       end
@@ -17,22 +17,19 @@ module Vagalume
     private
 
     def formatted_song_with_transaction(song, translated_song)
-      original_lyrics_lines = [song.name, ""]
-      translated_lyrics_lines = [translated_song.name, ""]
-
-      original_lyrics_lines += song.lyric.split("\n")
-      translated_lyrics_lines += translated_song.lyric.split("\n")
+      original_lyrics_lines = [song.name, ""] + song.lyric.split("\n")
+      translated_lyrics_lines = [translated_song.name, ""] + translated_song.lyric.split("\n")
 
       biggest_line_size = biggest_line_size(song.lyric)
-      output = "\n\n"
+      formatted_output = "\n\n"
 
       original_lyrics_lines.each_with_index do |original_line, index|
         separator = find_separator(biggest_line_size, original_line)
         translated_line = translated_lyrics_lines[index]
-        output += original_line + separator + translated_line + "\n"
+        formatted_output += original_line + separator + translated_line + "\n"
       end
 
-      output
+      formatted_output
     end
 
     def find_separator(biggest_line_size, lyric_line)
